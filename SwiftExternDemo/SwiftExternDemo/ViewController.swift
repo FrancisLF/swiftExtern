537332a8779de50c36198f9c94b6227c158763b1
francis <1102253945@qq.com>
2019-04-12 16:48:16 +0800

    add extern

---
+++ SwiftExternDemo/SwiftExternDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftExternDemo
+//
+//  Created by useradmin on 2019/4/12.
+//  Copyright © 2019年 francis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
